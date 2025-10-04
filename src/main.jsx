c1bcc57a47c7e8ccf18ab574edc351711097a206
ehasun15019 <ehasunulislam0202@gmail.com>
2025-10-04 21:58:37 +0600

    routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// import from react-router (installation)
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+// createBrowserRouter also import from react-router (installation)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* coming from react-router (installation) */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
